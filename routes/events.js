a3b9763e9c0a805da71693409fc92ede970cf84b
Taurean04 <ayobamitomi@gmail.com>
2021-04-10 08:18:45 +0100

    Updated Events Routes

--- routes/events.js
+++ routes/events.js
@@ -1,7 +1,10 @@
 var express = require('express');
+var { getAllEvents, addEvent, getByActor } = require('../controllers/events');
 var router = express.Router();
 
 // Routes related to event
-
+router.get('/', getAllEvents);
+router.post('/', addEvent);
+router.get('/actors/:id', getByActor);
 
 module.exports = router;
