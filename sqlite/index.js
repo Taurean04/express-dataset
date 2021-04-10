3a274fa9ae424b5828187a75a9f869d5a62ce3a8
Taurean04 <ayobamitomi@gmail.com>
2021-04-10 07:51:41 +0100

    sqlite connection

---
+++ sqlite/index.js
@@ -0,0 +1,32 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('dataset-db', null, null, {
+    host: 'localhost',
+    dialect: 'sqlite',
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+    },
+    storage: './db/database.sqlite3',
+    operatorsAliases: false,
+});
+
+sequelize
+.authenticate()
+.then(() => {
+    console.info('Database connection established');
+})
+.catch((err) => {
+    console.error('Unable to connect to database', err);
+});
+
+sequelize.sync()
+.then(() => {
+    console.info('Tables created successfully.');
+})
+.catch((err) => {
+    console.info('Error', err);
+});
+module.exports = sequelize;
