d1b060f5cfcdb102df8e33e51f63dc02b7aa6ae6
Taurean04 <ayobamitomi@gmail.com>
2021-04-10 08:17:04 +0100

    Updated Actor Routes

--- routes/actor.js
+++ routes/actor.js
@@ -1,6 +1,10 @@
 var express = require('express');
+const { getAllActors, getStreak, updateActor } = require('../controllers/actors');
 var router = express.Router();
 
 // Routes related to actor.
+router.get('/', getAllActors);
+router.put('/', updateActor);
+router.get('/streak', getStreak);
 
 module.exports = router;
